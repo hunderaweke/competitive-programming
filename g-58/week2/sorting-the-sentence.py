35c4dcb1a24ccf77af357bceca3c613056121a6d
Hundera Awoke <112551317+hunderaweke@users.noreply.github.com>
2024-01-01 21:52:56 +0300

    Added solution for Sorting the Sentence

---
+++ g-58/week2/sorting-the-sentence.py
@@ -0,0 +1,6 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        words = s.split()
+        words.sort(key=lambda x:x[-1])
+        words = [word[:len(word)-1] for word in words]
+        return ' '.join(words)

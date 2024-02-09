8af99d95c7704da52544a6c4a4e368ad6506d454
Hundera Awoke <112551317+hunderaweke@users.noreply.github.com>
2024-02-09 16:20:39 +0300

    Add solution for Palindrome Linked List

---
+++ prefix-sum/leetcode/palindrome-linked-list.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        nums = []
+        current = head
+        while current:
+            nums.append(current.val)
+            current = current.next
+        return nums == nums[::-1]

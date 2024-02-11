4189587f22dd0405093d75e31dfdf22a488fa55a
Hundera Awoke <112551317+hunderaweke@users.noreply.github.com>
2024-02-11 10:42:01 +0300

    Add solution for Reverse Linked List

--- prefix-sum/leetcode/reverse-linked-list.py
+++ prefix-sum/leetcode/reverse-linked-list.py
@@ -5,13 +5,11 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return None
-        newHead = head
-        if head.next:
-            newHead= self.reverseList(head.next)
-            head.next.next = head
-        head.next = None
-        return newHead        
-
-        
+        current = head
+        prev = None
+        while current:
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        return prev

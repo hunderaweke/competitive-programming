5479f605347ebc71dbb84e1e3c14eb1e95cd0556
Hundera Awoke <112551317+hunderaweke@users.noreply.github.com>
2024-02-21 19:28:58 +0300

    Add solution for Break a Palindrome

---
+++ prefix-sum/leetcode/break-a-palindrome.py
@@ -0,0 +1,13 @@
+class Solution:
+    def breakPalindrome(self, palindrome: str) -> str:
+        if len(palindrome) ==1:
+            return ""
+        changed = False
+        palindrome  = list(palindrome)
+        for i in range(len(palindrome)//2):
+            if palindrome[i] != "a":
+                palindrome[i] = "a"
+                return "".join(palindrome)
+
+        palindrome[-1] = "b"
+        return "".join(palindrome)

a112bf1044015ec8709653f910bbf75ec2c0c4e5
Hundera Awoke <112551317+hunderaweke@users.noreply.github.com>
2024-03-04 11:36:29 +0300

    Add solution for Combinations

--- prefix-sum/leetcode/combinations.py
+++ prefix-sum/leetcode/combinations.py
@@ -1,15 +1,17 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        res = []
+        result = []
 
-        def backtrack(start,comb):
-            if len(comb) == k:
-                res.append(comb.copy())
+
+        def dfs(start,curr_combination):
+            if len(curr_combination) == k:
+                result.append(curr_combination[:])
                 return
-            
-            for i in range(start,n+1):
-                comb.append(i)
-                backtrack(i+1,comb)
-                comb.pop()
-        backtrack(1,[])
-        return res
+    
+            for num in range(start,n+1):
+                curr_combination.append(num)
+                dfs(num+1,curr_combination)
+                curr_combination.pop()
+
+        dfs(1,[])
+        return result

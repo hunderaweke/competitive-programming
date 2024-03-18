9899d50fd46d65b34a8d4ee4d8bd79f51a703c88
Hundera Awoke <112551317+hunderaweke@users.noreply.github.com>
2024-03-18 07:14:49 +0300

    Add solution for H-Index II

---
+++ prefix-sum/leetcode/h-index-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        left,right = 0,n - 1
+        best = 0
+        while left <= right:
+            mid = left + (right-left) // 2
+            if citations[mid] == n - mid:
+                return n - mid
+            elif citations[mid] > n - mid:
+                right = mid-1
+            else:
+                left = mid+1
+        return n - left 
